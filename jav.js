ef692b12b0d504a8c795e05d7e85a850a8f027f6
bijas <bijas.backer.et@gmail.com>
2022-12-25 18:34:40 +0530

    simple-calculator

---
+++ jav.js
@@ -0,0 +1,11 @@
+function buttonclick(val){
+    document.getElementById("screen").value+=val
+}
+function cleardis(){
+    document.getElementById("screen").value=""
+}
+function equal(){
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById('screen').value=result
+}
